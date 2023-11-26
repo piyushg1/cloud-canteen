28ebfd8631a971497c4d448928ea3d0cfb7b37d5
Piyush <piyushat25@gmail.com>
2023-11-26 23:46:59 +0530

    search filter

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,15 +1,32 @@
 import ResCard from "./ResCard"
 import { d } from '../../data/config.js'
+import { useState } from "react"
 
 function Body() {
+    const [inp, setInp] = useState('')
+
+    const filteredData = d.filter((e) => {
+        return e.name.toLowerCase().includes(inp.toLowerCase());
+    });
+
+    const handleChange = (e) => {
+        setInp(e.target.value)
+    }
+
     return (
-        <div style={{ display: 'flex', justifyContent: 'space-around', flexWrap: 'wrap', margin: '2rem' }}>
-            {d.map((data) => {
-                return (
-                    <ResCard key={data.id} {...data} />
-                )
-            })}
-        </div>
+        <>
+            <input type="text" value={inp} onChange={(e) => handleChange(e)} placeholder="search" style={{
+                padding: '10px', borderRadius: '4px', border: '1px solid #ccc', marginTop: '4.2rem', marginLeft: '5rem'
+            }} />
+
+            <div style={{ display: 'flex', justifyContent: 'space-around', flexWrap: 'wrap', margin: '2rem' }}>
+                {filteredData.map((data) => {
+                    return (
+                        <ResCard key={data.id} {...data} />
+                    )
+                })}
+            </div>
+        </>
     )
 }
 
